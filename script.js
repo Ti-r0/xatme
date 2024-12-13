0bd4e50fa85574279bfe1099d65d95eb35eee3a9
Ti-r0 <72718988+Ti-r0@users.noreply.github.com>
2024-12-13 06:13:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready((e) => {
+  const mainIcon =
+    "https://xatimg.com/image/u6GsF2EB4PP7.jpg";
+  const username = "Mikay";
+  const profileDescription = "(505050505)";
+  const profileLove = "Wolfie";
+  const profileLink = "";
+
+  $("#profile").show();
+  $(".profile-icon").attr("src", mainIcon);
+  $(".usernameText").text(username);
+  $(".descriptionText").text(profileDescription);
+  $(".loveText").text(profileLove);
+  $(".profileLink").attr("href", profileLink);
+  $(".profileLink").text(profileLink);
+
+  $("[target]").click((a) => {
+    const target = $(a.currentTarget).attr("target");
+    $(".page-hidden").hide();
+    $("#" + target).toggle();
+
+    if (target.includes("story")) $("#top-menu-bar").hide();
+    else $("#top-menu-bar").show();
+  });
+});
