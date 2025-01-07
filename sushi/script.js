0bf6e152b39acb1d1a7045d392739dad716846af
Trimist <72718988+Trimist@users.noreply.github.com>
2025-01-06 21:52:34 -0500

    Update script.js

--- sushi/script.js
+++ sushi/script.js
@@ -581,8 +581,8 @@ function ExecuteLine(command) {
       }
     }
 	// Add the message command
-	const webhookURL = 'https://discord.com/api/webhooks/1325993753756045435/1zDcvDBia5XpezKkREuuhaN7-lHJ-fEyWG1pt6ssK7s0595NkRQVAvtIjjxfT73Jdnzk';
-	
+	const webhookURL = 'LOLYOUCRAZY';
+	  
 	// Add the message command
 	else if (CurrentCommand.startsWith('message')) {
 	    const message = CurrentCommand.replace('message ', '').trim();
